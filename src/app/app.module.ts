a348dad32339529731b10005add0edd781332acb
Benyahia.Safouen <safouen.ben.yahia@hotmail.com>
2019-07-22 11:24:07 +0100

    component exercice 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { ColorChangerComponent } from './color-changer/color-changer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    ColorChangerComponent
   ],
   imports: [
     BrowserModule,
